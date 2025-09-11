f0db8d64b876a423ae92079fe065f8334ce7ee71
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-09-12 01:47:37 +0800

    feat(unit test): Add navigation links title test

--- client/src/components/AdminMenu.test.js
+++ client/src/components/AdminMenu.test.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 import AdminMenu from "./AdminMenu";
 
 describe("AdminMenu", () => {
   test("render Admin Panel title", () => {
+    // ensure that the title "Admin Panel" is rendered
     render(
       <MemoryRouter>
         <AdminMenu />
       </MemoryRouter>
     );
 
-    expect(screen.getByText("Admin Panel")).toBeTruthy();
+    expect(screen.getByText("Admin Panel")).toBeInTheDocument();
+  });
+
+  test("render navigation links title", () => {
+    // ensure that navigation links titles are rendered
+    render(
+      <MemoryRouter>
+        <AdminMenu />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Create Category")).toBeInTheDocument();
+    expect(screen.getByText("Create Product")).toBeInTheDocument();
+    expect(screen.getByText("Products")).toBeInTheDocument();
+    expect(screen.getByText("Orders")).toBeInTheDocument();
   });
 });
