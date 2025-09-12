71d7c6b527da44f28cb1f3717910f0766c451c0c
wr1159 <chuweiron18@gmail.com>
2025-09-12 11:04:52 -0400

    chore(frontend jest): Update tests for all tests files except from _* folders

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,11 @@ module.exports = {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: ["<rootDir>/client/src/!(_*)/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/!(_*)/**"],
   coverageThreshold: {
     global: {
       lines: 90,
