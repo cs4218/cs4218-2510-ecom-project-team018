e0252726ebd44f48541e5ec57cf5bed6c94bebcf
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-09-16 02:31:15 +0800

    feat(unit test): Create base admin dashboard test file

---
+++ client/src/pages/admin/AdminDashboard.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useAuth } from "../../context/auth";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import AdminDashboard from "./AdminDashboard";
+
+describe("Admin Dashboard", () => {});
