a324bb708e0c711dd82d3bc3b00e3e0e11451cd9
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2025-09-22 22:09:47 +0800

    Completed Test for Order Model

--- models/orderModel.js
+++ models/orderModel.js
@@ -2,21 +2,41 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    products: [
-      {
-        type: mongoose.ObjectId,
-        ref: "Products",
+    products: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Prodcuts",
+          required: true,
+        }
+      ],
+      required: true,
+      validate: {
+        validator: function (v) {
+          return Array.isArray(v) && v.length > 0;
+        },
+        message: "Order must contain at least one product.",
       },
-    ],
-    payment: {},
+    },
+
+    payment: {
+      type: {
+        success: { type: Boolean, required: true}
+      },
+      required: true
+    },
+
     buyer: {
       type: mongoose.ObjectId,
       ref: "users",
+      required: true,
     },
+
     status: {
       type: String,
       default: "Not Processed",
       enum: ["Not Processed", "Processing", "Shipped", "delivered", "cancel"],
+      required: true,
     },
   },
   { timestamps: true }
--- models/orderModel.test.js
+++ models/orderModel.test.js
@@ -7,7 +7,7 @@ const MOCK_ORDER_DATA = {
         new mongoose.Types.ObjectId(),
     ],
     payment: {
-        success: True
+        success: true
     },
     buyer: new mongoose.Types.ObjectId(),
     status: "Processing",
@@ -16,6 +16,76 @@ const MOCK_ORDER_DATA = {
 describe("Order schema unit tests", () => {
     it("should validate a correct order", () => {
         const order = new Order(MOCK_ORDER_DATA);
+        const error = order.validateSync();
 
+        expect(error).toBeUndefined();
     })
+
+    it("should fail validation when required fields are missing", () => {
+        const order = new Order({});
+        const error = order.validateSync();
+
+        expect(error).toBeDefined();
+        expect(error.errors.payment).toBeDefined();
+        expect(error.errors.buyer).toBeDefined();
+        expect(error.errors.products).toBeDefined();
+        // Status will be present since it has a Default Value
+    })
+
+    it("should fail when status is not in the possible status enum values", () => {
+        const order = new Order({
+            ...MOCK_ORDER_DATA,
+            status: "UnknownStatus",
+        });
+        
+        const error = order.validateSync();
+        expect(error.errors.status).toBeDefined();
+    })
+
+    it("should fail when products array is empty", () => {
+        const order = new Order({
+            ...MOCK_ORDER_DATA,
+            products: [],
+        });
+
+        const error = order.validateSync();
+        expect(error.errors.products).toBeDefined();
+        expect(error.errors.products.message).toBe("Order must contain at least one product.");
+    })
+
+    it("should fail when products contain invalid ObjectId", () => {
+        const order = new Order({
+            ...MOCK_ORDER_DATA,
+            products: ["not-a-valid-objectid"],
+        });
+        const error = order.validateSync();
+        expect(error.errors["products.0"]).toBeDefined();
+    })
+
+    it("should fail when payment.success is missing", () => {
+        const order = new Order({
+            ...MOCK_ORDER_DATA,
+            payment: {},
+        });
+        const error = order.validateSync();
+        expect(error.errors["payment.success"]).toBeDefined();
+    });
+
+    it("should fail when payment.success is not Boolean", () => {
+        const order = new Order({
+            ...MOCK_ORDER_DATA,
+            payment: { success: "yes" },
+        });
+        const error = order.validateSync();
+        expect(error.errors["payment.success"]).toBeDefined();
+    });
+
+    it("should fail when buyer is not an ObjectId", () => {
+        const order = new Order({
+            ...MOCK_ORDER_DATA,
+            buyer: "12345",
+        });
+        const error = order.validateSync();
+        expect(error.errors.buyer).toBeDefined();
+    });
 })
