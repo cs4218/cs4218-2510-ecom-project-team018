254b8cd2966eb0be07b47c779f975bd52beffd76
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-09-25 21:29:09 +0800

    feat(unit test): Implement "successfully create a category" test

---
+++ controllers/categoryController.test.js
@@ -0,0 +1,59 @@
+import categoryModel from "../models/categoryModel.js";
+import slugify from "slugify";
+import { createCategoryController } from "./categoryController.js";
+
+// sample data
+const CATEGORY_NAME_0 = "Electronics";
+const CATEGORY_SLUG_0 = "electronics";
+
+const CATEGORY_NAME_1 = "Books";
+const CATEGORY_SLUG_1 = "books";
+
+// status codes
+const SUCCESS_STATUS = 200;
+const CREATED_STATUS = 201;
+const SERVER_ERROR_STATUS = 500;
+
+// mocks
+jest.mock("../models/categoryModel.js"); // to not hit real database
+jest.mock("slugify");
+
+describe("Creating a category", () => {
+  let req, res;
+
+  beforeEach(() => {
+    req = { body: {} };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+
+    jest.clearAllMocks();
+  });
+
+  test("successfully create a category", async () => {
+    /* arrange */
+    req.body = { name: CATEGORY_NAME_1 };
+    categoryModel.findOne.mockResolvedValue(null); // no existing category with the same name
+    slugify.mockReturnValue(CATEGORY_SLUG_1);
+
+    // mock saving into db
+    const saveMock = jest
+      .fn()
+      .mockResolvedValue({ name: CATEGORY_NAME_1, slug: CATEGORY_SLUG_1 });
+    categoryModel.mockImplementation(() => ({ save: saveMock }));
+
+    /* act*/
+    await createCategoryController(req, res);
+
+    /* assert */
+    expect(slugify).toHaveBeenCalledWith(CATEGORY_NAME_1);
+    expect(saveMock).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(CREATED_STATUS);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      message: "New category created",
+      category: { name: CATEGORY_NAME_1, slug: CATEGORY_SLUG_1 },
+    });
+  });
+});
