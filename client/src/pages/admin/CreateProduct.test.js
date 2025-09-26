2acd87b42f43df7cb8d7ababe3b67976a18339ae
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-09-26 19:21:02 +0800

    feat(unit test): Implement "submits successfully with valid data" test

--- client/src/pages/admin/CreateProduct.test.js
+++ client/src/pages/admin/CreateProduct.test.js
@@ -1,20 +1,17 @@
 import React from "react";
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  within,
-} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 import CreateProduct from "./CreateProduct";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-// mocks
+/* mocks */
 jest.mock("axios");
 jest.mock("react-hot-toast");
+
+// mock photo preview
+global.URL.createObjectURL = jest.fn(() => "mocked-url");
 
 // mock components
 jest.mock("../../components/Layout", () => (props) => (
@@ -25,10 +22,36 @@ jest.mock("../../components/AdminMenu", () => () => (
   <div data-testid="AdminMenu" />
 ));
 
+// mock navigate (page auto navigates to '/dashboard/admin/products' upon successful product creation)
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
 // sample data
 const SAMPLE_CATEGORIES = [
   { _id: "1", name: "Electronics" },
   { _id: "2", name: "Books" },
+];
+
+const SHIPPING = {
+  YES: "Yes",
+  NO: "No",
+};
+
+const SAMPLE_INPUTS = [
+  {
+    category: "Electronics",
+    photo: new File(["dummy content"], "test-photo.png", {
+      type: "image/png",
+    }),
+    name: "Test Product",
+    description: "Test description",
+    price: "50",
+    quantity: "10",
+    shipping: SHIPPING.NO,
+  },
 ];
 
 beforeEach(() => {
@@ -105,6 +128,83 @@ describe("Actions - getAllCategory() function", () => {
       SAMPLE_CATEGORIES.forEach((cat) => {
         expect(screen.getByText(cat.name)).toBeInTheDocument();
       });
+    });
+  });
+});
+
+describe("Actions - handleCreate() function", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: { success: true, category: SAMPLE_CATEGORIES },
+    });
+  });
+
+  test("submits successfully with valid data", async () => {
+    // mock successful product creation return from backend
+    // note: in the handleCreate() function, data.success == false triggers success (yes its the expected behaviour)
+    axios.post.mockResolvedValue({ data: { success: false } });
+
+    render(
+      <MemoryRouter>
+        <CreateProduct />
+      </MemoryRouter>
+    );
+
+    // wait for categories to load
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalled();
+    });
+
+    // select category
+    const selectCategory = screen.getAllByRole("combobox")[0];
+    fireEvent.mouseDown(selectCategory);
+    await waitFor(() => {
+      SAMPLE_CATEGORIES.forEach((cat) => {
+        expect(screen.getByText(cat.name)).toBeInTheDocument();
+      });
+    });
+    fireEvent.click(screen.getByText(SAMPLE_INPUTS[0].category));
+
+    //  picture
+    const photoInput = screen.getByLabelText(/upload photo/i);
+    fireEvent.change(photoInput, {
+      target: { files: [SAMPLE_INPUTS[0].photo] },
+    });
+    // wait for photo preview to appear
+    await waitFor(() => {
+      expect(screen.getByAltText(/product_photo/i)).toBeInTheDocument();
+    });
+    // label to display the file name
+    expect(screen.getByText(SAMPLE_INPUTS[0].photo.name)).toBeInTheDocument();
+
+    // fill inputs
+    fireEvent.change(screen.getByPlaceholderText(/write a name/i), {
+      target: { value: SAMPLE_INPUTS[0].name },
+    });
+    fireEvent.change(screen.getByPlaceholderText(/write a description/i), {
+      target: { value: SAMPLE_INPUTS[0].description },
+    });
+    fireEvent.change(screen.getByPlaceholderText(/write a price/i), {
+      target: { value: SAMPLE_INPUTS[0].price },
+    });
+    fireEvent.change(screen.getByPlaceholderText(/write a quantity/i), {
+      target: { value: SAMPLE_INPUTS[0].quantity },
+    });
+
+    // select shipping
+    const selectShipping = screen.getAllByRole("combobox")[1];
+    fireEvent.mouseDown(selectShipping);
+    fireEvent.click(screen.getByText(SAMPLE_INPUTS[0].shipping));
+
+    // click 'create product' button
+    fireEvent.click(screen.getByRole("button", { name: /create product/i }));
+
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalled();
+      expect(toast.success).toHaveBeenCalledWith(
+        "Product Created Successfully"
+      );
+      expect(mockNavigate).toHaveBeenCalledWith("/dashboard/admin/products");
     });
   });
 });
