9d27192d2f4c1b6a2bf993c6d36202dda7a3ff1f
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-10-03 18:35:15 +0800

    feat(unit test): Implement "return error when missing category field" test

--- controllers/productController.test.js
+++ controllers/productController.test.js
@@ -278,4 +278,15 @@ describe("Product Controller - updating a product", () => {
       expect.objectContaining({ error: "Price is required" })
     );
   });
+
+  test("return error when missing category field", async () => {
+    req.fields.category = "";
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(SERVER_ERROR_STATUS);
+    expect(res.send).toHaveBeenCalledWith(
+      expect.objectContaining({ error: "Category is required" })
+    );
+  });
 });
