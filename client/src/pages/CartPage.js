bd59089a2c565d1ae61c0442798237efc8bf331e
Chen-Kuei <rubychen0427@gmail.com>
2025-10-19 14:50:36 +0800

    fix double spaces

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -132,7 +132,7 @@ const CartPage = () => {
             <h1 className="text-center bg-light p-2 mb-1">
               {!auth?.user
                 ? "Hello Guest"
-                : `Hello  ${auth?.token && auth?.user?.name}`}
+                : `Hello ${auth?.token && auth?.user?.name}`}
               <p className="text-center">
                 {cart?.length
                   ? `You Have ${cart.length} items in your cart ${
