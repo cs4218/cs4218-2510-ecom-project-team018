6a12578a683e56950f2a2f55496e7093420566d0
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-10-21 18:18:07 +0800

    feat(UI test): update tests

--- client/src/pages/admin/CreateProduct.spec.js
+++ client/src/pages/admin/CreateProduct.spec.js
@@ -89,230 +89,168 @@ test.describe("Create Category Page", () => {
     await categoryModel.deleteMany({});
   });
 
-  // test("successfully render all UI elements", async ({ page }) => {
-  //   // heading
-  //   await expect(page.locator("h1")).toContainText("Create Product");
+  test("successfully render all UI elements", async ({ page }) => {
+    // heading
+    await expect(page.locator("h1")).toContainText("Create Product");
 
-  //   // fields
-  //   await expect(page.getByRole("main")).toContainText("Select a category");
-  //   await expect(page.getByText("Upload Photo")).toBeVisible();
-  //   await expect(page.getByPlaceholder("Write a name")).toBeVisible();
-  //   await expect(page.getByPlaceholder("Write a description")).toBeVisible();
-  //   await expect(page.getByPlaceholder("Write a Price")).toBeVisible();
-  //   await expect(page.getByPlaceholder("Write a quantity")).toBeVisible();
-  //   await expect(page.getByRole("main")).toContainText("Select Shipping");
+    // fields
+    await expect(page.getByRole("main")).toContainText("Select a category");
+    await expect(page.getByText("Upload Photo")).toBeVisible();
+    await expect(page.getByPlaceholder("Write a name")).toBeVisible();
+    await expect(page.getByPlaceholder("Write a description")).toBeVisible();
+    await expect(page.getByPlaceholder("Write a Price")).toBeVisible();
+    await expect(page.getByPlaceholder("Write a quantity")).toBeVisible();
+    await expect(page.getByRole("main")).toContainText("Select Shipping");
 
-  //   // button
-  //   await expect(
-  //     page.getByRole("button", { name: "CREATE PRODUCT" })
-  //   ).toBeVisible();
-  // });
+    // button
+    await expect(
+      page.getByRole("button", { name: "CREATE PRODUCT" })
+    ).toBeVisible();
+  });
 
-  // test("should populate category dropdown and allow selection", async ({
-  //   page,
-  // }) => {
-  //   const selectCategory = page.locator(".ant-select-dropdown");
+  test("should populate category dropdown and allow selection", async ({
+    page,
+  }) => {
+    const selectCategory = page.locator(".ant-select-dropdown");
 
-  //   // open dropdown
-  //   await page.click(".ant-select");
-  //   await expect(selectCategory).toBeVisible();
+    // open dropdown
+    await page.click(".ant-select");
+    await expect(selectCategory).toBeVisible();
 
-  //   // assert that existing categories are visible in the drop down
-  //   await expect(
-  //     selectCategory.getByText(SAMPLE_CATEGORIES[0].name)
-  //   ).toBeVisible();
-  //   await expect(
-  //     selectCategory.getByText(SAMPLE_CATEGORIES[1].name)
-  //   ).toBeVisible();
+    // assert that existing categories are visible in the drop down
+    await expect(
+      selectCategory.getByText(SAMPLE_CATEGORIES[0].name)
+    ).toBeVisible();
+    await expect(
+      selectCategory.getByText(SAMPLE_CATEGORIES[1].name)
+    ).toBeVisible();
 
-  //   // select "electronics" option
-  //   await selectCategory.getByText(SAMPLE_CATEGORIES[0].name).click();
+    // select "electronics" option
+    await selectCategory.getByText(SAMPLE_CATEGORIES[0].name).click();
 
-  //   // verify "electronics" has been selected
-  //   await expect(
-  //     page.locator(".ant-select-selection-item", {
-  //       hasText: SAMPLE_CATEGORIES[0].name,
-  //     })
-  //   ).toBeVisible();
-  // });
+    // verify "electronics" has been selected
+    await expect(
+      page.locator(".ant-select-selection-item", {
+        hasText: SAMPLE_CATEGORIES[0].name,
+      })
+    ).toBeVisible();
+  });
 
-  // test("should show image preview after uploading image", async ({ page }) => {
-  //   // test image
-  //   const filePath = path.join(
-  //     __dirname,
-  //     "../../../public/images/placeholder.png"
-  //   );
+  test("should show image preview after uploading image", async ({ page }) => {
+    // test image
+    const filePath = path.join(
+      __dirname,
+      "../../../public/images/placeholder.png"
+    );
 
-  //   // click 'upload photo' & input test image
-  //   await page.getByText("Upload Photo").click();
-  //   await page.locator('input[type="file"]').setInputFiles(filePath);
+    // click 'upload photo' & input test image
+    await page.getByText("Upload Photo").click();
+    await page.locator('input[type="file"]').setInputFiles(filePath);
 
-  //   // 'upload photo' button to now show the file's name
-  //   await expect(page.getByText("placeholder.png")).toBeVisible();
-  //   await expect(page.locator("label")).toContainText("placeholder.png");
+    // 'upload photo' button to now show the file's name
+    await expect(page.getByText("placeholder.png")).toBeVisible();
+    await expect(page.locator("label")).toContainText("placeholder.png");
 
-  //   // preview is visible
-  //   await expect(page.locator("img[alt='product_photo']")).toBeVisible();
-  // });
+    // preview is visible
+    await expect(page.locator("img[alt='product_photo']")).toBeVisible();
+  });
 
-  // test("missing name should return error message", async ({ page }) => {
-  //   // leave form empty and submit
-  //   await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+  test("missing fields should return their error messages", async ({
+    page,
+  }) => {
+    // leave form empty & submit
+    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+    // assert error toast
+    await expect(page.getByRole("main")).toContainText(/category is required/i);
 
-  //   // assert error toast
-  //   await expect(page.getByRole("main")).toContainText(/name is required/i);
-  // });
+    // select category
+    const selectCategory = page.locator(".ant-select-dropdown");
+    await page.click(".ant-select");
+    await selectCategory.getByText(SAMPLE_CATEGORIES[0].name).click();
+    // submit
+    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+    // assert error toast
+    await expect(page.getByRole("main")).toContainText(/name is required/i);
 
-  // test("missing description should return error message", async ({ page }) => {
-  //   // fill in product name
-  //   await page
-  //     .getByRole("textbox", { name: "Write a name" })
-  //     .fill("Test Product");
-
-  //   // submit
-  //   await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
-
-  //   // assert error toast
-  //   await expect(page.getByRole("main")).toContainText(
-  //     /description is required/i
-  //   );
-  // });
-
-  test("missing price should return error message", async ({ page }) => {
     // fill in product name
     await page
       .getByRole("textbox", { name: "Write a name" })
       .fill("Test Product");
+    // submit
+    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+    // assert error toast
+    await expect(page.getByRole("main")).toContainText(
+      /description is required/i
+    );
+
     // fill in product description
     await page
       .getByRole("textbox", { name: "Write a description" })
       .fill("Test Product Description");
+    // submit
+    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+    // assert error toast
+    await expect(page.getByRole("main")).toContainText(/price is required/i);
+
+    // fill in price
+    await page.getByPlaceholder("Write a Price").fill("100");
+    // submit
+    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+    // assert error toast
+    await expect(page.getByRole("main")).toContainText(/quantity is required/i);
+
+    // fill in quantity
+    await page.getByPlaceholder("Write a quantity").fill("10");
+    // submit
+    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+    // assert error toast
+    await expect(page.getByRole("main")).toContainText(/shipping is required/i);
+  });
+
+  test("successfully create a product and navigates to products page", async ({
+    page,
+  }) => {
+    // select "electronics" category
+    const selectCategory = page.locator(".ant-select-dropdown");
+    await page.click(".ant-select");
+    await selectCategory.getByText(SAMPLE_CATEGORIES[0].name).click();
+
+    // fill in fields
+    await page.getByRole("textbox", { name: "Write a name" }).click();
+    await page
+      .getByRole("textbox", { name: "Write a name" })
+      .fill("Test Product");
+    await page.getByRole("textbox", { name: "Write a description" }).click();
+    await page
+      .getByRole("textbox", { name: "Write a description" })
+      .fill("Test Product Description");
+    await page.getByPlaceholder("Write a Price").click();
+    await page.getByPlaceholder("Write a Price").fill("100");
+    await page.getByPlaceholder("Write a quantity").click();
+    await page.getByPlaceholder("Write a quantity").fill("10");
+
+    // select "no" shipping
+    const selects = page.locator(".ant-select");
+    await selects.nth(1).click();
+    await page.getByText("No").click();
+
+    // upload image
+    const filePath = path.join(
+      __dirname,
+      "../../../public/images/placeholder.png"
+    );
+    await page.getByText("Upload Photo").click();
+    await page.locator('input[type="file"]').setInputFiles(filePath);
 
     // submit
     await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
 
-    // assert error toast
-    await expect(page.getByRole("main")).toContainText(/price is required/i);
+    // assert success toast
+    await expect(page.getByRole("main")).toContainText(
+      "Product Created Successfully"
+    );
+
+    // assert navigation
+    await expect(page).toHaveURL(/\/dashboard\/admin\/products/);
   });
-
-  // test("successfully create a product and navigates to products page", async ({
-  //   page,
-  // }) => {
-  //   // mock create product API
-  //   await page.route("**/api/v1/product/create-product", async (route) => {
-  //     await route.fulfill({
-  //       status: SUCCESS_STATUS,
-  //       body: JSON.stringify({ success: true }),
-  //     });
-  //   });
-
-  //   // select "electronics" category
-  //   const selectCategory = page.locator(".ant-select-dropdown");
-  //   await page.click(".ant-select");
-  //   await selectCategory.getByText(SAMPLE_CATEOGRIES[0].name).click();
-
-  //   // fill in fields
-  //   await page.getByRole("textbox", { name: "Write a name" }).click();
-  //   await page
-  //     .getByRole("textbox", { name: "Write a name" })
-  //     .fill("Test Product");
-  //   await page.getByRole("textbox", { name: "Write a description" }).click();
-  //   await page
-  //     .getByRole("textbox", { name: "Write a description" })
-  //     .fill("Test Product Description");
-  //   await page.getByPlaceholder("Write a Price").click();
-  //   await page.getByPlaceholder("Write a Price").fill("100");
-  //   await page.getByPlaceholder("Write a quantity").click();
-  //   await page.getByPlaceholder("Write a quantity").fill("10");
-
-  //   // select "no" shipping
-  //   const selects = page.locator(".ant-select");
-  //   await selects.nth(1).click();
-  //   await page.getByText("No").click();
-
-  //   // upload image
-  //   const filePath = path.join(
-  //     __dirname,
-  //     "../../../public/images/placeholder.png"
-  //   );
-  //   await page.getByText("Upload Photo").click();
-  //   await page.locator('input[type="file"]').setInputFiles(filePath);
-
-  //   // submit
-  //   await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
-
-  //   // assert success toast
-  //   await expect(page.getByRole("main")).toContainText(
-  //     "Product Created Successfully"
-  //   );
-
-  //   // assert navigation
-  //   await expect(page).toHaveURL(/\/dashboard\/admin\/products/);
-  // });
-
-  // test("should show error toast if API fails", async ({ page }) => {
-  //   // mock POST route to fail
-  //   await page.route("**/api/v1/product/create-product", async (route) => {
-  //     await route.fulfill({
-  //       status: INTERNAL_SERVER_ERROR_STATUS,
-  //       contentType: "application/json",
-  //       body: JSON.stringify({ success: false, message: "Server Error" }),
-  //     });
-  //   });
-
-  //   // fill in only required fields
-  //   const selectCategory = page.locator(".ant-select-dropdown");
-  //   await page.click(".ant-select");
-  //   await selectCategory.getByText(SAMPLE_CATEOGRIES[0].name).click();
-  //   await page.click(".ant-select");
-  //   await page.getByRole("textbox", { name: "Write a name" }).click();
-  //   await page
-  //     .getByRole("textbox", { name: "Write a name" })
-  //     .fill("Broken Product");
-  //   await page.getByRole("textbox", { name: "Write a description" }).click();
-  //   await page
-  //     .getByRole("textbox", { name: "Write a description" })
-  //     .fill("This will fail");
-  //   await page.getByPlaceholder("Write a Price").click();
-  //   await page.getByPlaceholder("Write a Price").fill("1");
-  //   await page.getByPlaceholder("Write a quantity").click();
-  //   await page.getByPlaceholder("Write a quantity").fill("1");
-
-  //   // submit
-  //   await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
-
-  //   // assert error toast
-  //   await expect(page.getByRole("main")).toContainText("Something went wrong");
-  // });
-
-  // test("missing required fields should return an error", async ({ page }) => {
-  //   // mock POST route to fail
-  //   await page.route("**/api/v1/product/create-product", async (route) => {
-  //     await route.fulfill({
-  //       status: INTERNAL_SERVER_ERROR_STATUS,
-  //       contentType: "application/json",
-  //       body: JSON.stringify({ success: false, message: "Server Error" }),
-  //     });
-  //   });
-
-  //   // partially fill out form (no price & quantity)
-  //   const selectCategory = page.locator(".ant-select-dropdown");
-  //   await page.click(".ant-select");
-  //   await selectCategory.getByText(SAMPLE_CATEOGRIES[0].name).click();
-  //   await page.click(".ant-select");
-  //   await page.getByRole("textbox", { name: "Write a name" }).click();
-  //   await page
-  //     .getByRole("textbox", { name: "Write a name" })
-  //     .fill("Half filled form's product");
-  //   await page.getByRole("textbox", { name: "Write a description" }).click();
-  //   await page
-  //     .getByRole("textbox", { name: "Write a description" })
-  //     .fill("Half filled form's description");
-
-  //   // submit
-  //   await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
-
-  //   // assert error toast
-  //   await expect(page.getByRole("main")).toContainText("Something went wrong");
-  // });
 });
